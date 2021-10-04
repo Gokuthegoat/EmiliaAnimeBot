8683aa5883b59fd1ef3538d80422d161ef27ca36
Aarukami <91074890+Aarukami@users.noreply.github.com>
2021-10-04 16:03:02 +0530

    Update sample_config.py

--- EmiliaAnimeBot/sample_config.py
+++ EmiliaAnimeBot/sample_config.py
@@ -18,9 +18,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Izumi"
-    SUPPORT_CHAT = 'TangentChat'  #Your own group for support, do not add the @
+    OWNER_ID = 1537076718  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "TheDivineGod"
+    SUPPORT_CHAT = 'lelouchsupportchat'  #Your own group for support, do not add the @
     JOIN_LOGGER = -1234567789  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -123456789  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
